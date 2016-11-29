8ae2dce03274bc1236150a91d853b79d7ff9cb23
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 10:44:58 -0800

    Add error class and wrap module

--- lib/git_hub_integration/token_encryption.rb
+++ lib/git_hub_integration/token_encryption.rb
@@ -1,26 +1,30 @@
-module TokenEncryption
-  def self.rbnacl_secret
-    ENV["RBNACL_SECRET"] ||
-      raise("No RBNACL_SECRET environmental variable set")
-  end
+class RbnaclSecretMissing < StandardError; end
 
-  def self.rbnacl_secret_bytes
-    rbnacl_secret.unpack("m0").first
-  end
+module GitHubIntegration
+  module TokenEncryption
+    def self.rbnacl_secret
+      ENV["RBNACL_SECRET"] ||
+        raise(RbnaclSecretMissing, "No RBNACL_SECRET environmental variable set")
+    end
 
-  def self.rbnacl_simple_box
-    @rbnacl_simple_box ||=
-      RbNaCl::SimpleBox.from_secret_key(rbnacl_secret_bytes)
-  end
+    def self.rbnacl_secret_bytes
+      rbnacl_secret.unpack("m0").first
+    end
 
-  def self.decrypt_value(value)
-    rbnacl_simple_box.decrypt(Base64.decode64(value))
-  rescue RbNaCl::CryptoError, NoMethodError
-    nil
-  end
+    def self.rbnacl_simple_box
+      @rbnacl_simple_box ||=
+        RbNaCl::SimpleBox.from_secret_key(rbnacl_secret_bytes)
+    end
 
-  def self.encrypt_value(value)
-    return if value.nil? || value == ""
-    Base64.encode64(rbnacl_simple_box.encrypt(value)).chomp
+    def self.decrypt_value(value)
+      rbnacl_simple_box.decrypt(Base64.decode64(value))
+    rescue RbNaCl::CryptoError, NoMethodError
+      nil
+    end
+
+    def self.encrypt_value(value)
+      return if value.nil? || value == ""
+      Base64.encode64(rbnacl_simple_box.encrypt(value)).chomp
+    end
   end
 end
