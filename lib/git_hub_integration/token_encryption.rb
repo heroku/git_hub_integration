d4398bb12327a6039b2920d331ec397de037d30a
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 10:30:16 -0800

    Add token encryption module

---
+++ lib/git_hub_integration/token_encryption.rb
@@ -0,0 +1,26 @@
+module TokenEncryption
+  def self.rbnacl_secret
+    ENV["RBNACL_SECRET"] ||
+      raise("No RBNACL_SECRET environmental variable set")
+  end
+
+  def self.rbnacl_secret_bytes
+    rbnacl_secret.unpack("m0").first
+  end
+
+  def self.rbnacl_simple_box
+    @rbnacl_simple_box ||=
+      RbNaCl::SimpleBox.from_secret_key(rbnacl_secret_bytes)
+  end
+
+  def self.decrypt_value(value)
+    rbnacl_simple_box.decrypt(Base64.decode64(value))
+  rescue RbNaCl::CryptoError, NoMethodError
+    nil
+  end
+
+  def self.encrypt_value(value)
+    return if value.nil? || value == ""
+    Base64.encode64(rbnacl_simple_box.encrypt(value)).chomp
+  end
+end
