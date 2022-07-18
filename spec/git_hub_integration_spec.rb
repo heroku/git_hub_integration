9809a28620466eb82fb4a72363e3258c4f4c3718
Mauricio Gomes <mauricio@edge14.com>
2022-07-18 16:44:13 -0400

    Allow env var to be set as thread local variables

--- spec/git_hub_integration_spec.rb
+++ spec/git_hub_integration_spec.rb
@@ -48,4 +48,71 @@ describe GitHubIntegration do
     decrypted_value = GitHubIntegration::TokenEncryption.decrypt_value(token)
     expect(decrypted_value).to eql("test")
   end
+
+  describe "fetching GitHub App tokens" do
+    describe ".github_integration_id" do
+      it "should fetch from the environment variable GITHUB_INTEGRATION_ID and cast it as an integer if set" do
+        with_environment("GITHUB_INTEGRATION_ID" => "789") do
+          expect(GitHubIntegration.github_integration_id).to eq(789)
+        end
+      end
+
+      it "should return nil if GITHUB_INTEGRATION_ID is not set and a thread-var is not set" do
+        with_environment("GITHUB_INTEGRATION_ID" => nil) do
+          expect(GitHubIntegration.github_integration_id).to be_nil
+        end
+      end
+
+      it "should return the value of the thread-var if set" do
+        with_environment("GITHUB_INTEGRATION_ID" => nil) do
+          Thread.current[:github_integration_id] = 789
+          expect(GitHubIntegration.github_integration_id).to eq(789)
+        end
+      end
+    end
+
+    describe ".github_private_key" do
+      it "should fetch from the environment variable GITHUB_PRIVATE_KEY if set" do
+        # Requires a valid RSA private key
+        expect(ENV["GITHUB_PRIVATE_KEY"]).to_not be_nil
+        expect(GitHubIntegration.github_private_key.to_s).to eq(ENV["GITHUB_PRIVATE_KEY"])
+      end
+
+      it "should return nil if GITHUB_PRIVATE_KEY is not set and a thread-var is not set" do
+        with_environment("GITHUB_PRIVATE_KEY" => nil) do
+          expect(GitHubIntegration.github_private_key).to be_nil
+        end
+      end
+
+      it "should return the value of the thread-var if set" do
+        # Requires a valid RSA private key
+        private_key = ENV["GITHUB_PRIVATE_KEY"]
+        with_environment("GITHUB_PRIVATE_KEY" => nil) do
+          Thread.current[:github_private_key] = private_key
+          expect(GitHubIntegration.github_private_key.to_s).to eq(private_key)
+        end
+      end
+    end
+
+    describe ".github_installation_id" do
+      it "should fetch from the environment variable GITHUB_INTEGRATION_APPLICATION_ID if set" do
+        with_environment("GITHUB_INTEGRATION_APPLICATION_ID" => "789") do
+          expect(GitHubIntegration.github_installation_id).to eq("789")
+        end
+      end
+
+      it "should return nil if GITHUB_INTEGRATION_APPLICATION_ID is not set and a thread-var is not set" do
+        with_environment("GITHUB_INTEGRATION_APPLICATION_ID" => nil) do
+          expect(GitHubIntegration.github_installation_id).to be_nil
+        end
+      end
+
+      it "should return the value of the thread-var if set" do
+        with_environment("GITHUB_INTEGRATION_APPLICATION_ID" => nil) do
+          Thread.current[:github_installation_id] = "789"
+          expect(GitHubIntegration.github_installation_id).to eq("789")
+        end
+      end
+    end
+  end
 end
